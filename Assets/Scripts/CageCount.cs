802af42ce1a3769282b08f818efdc5793d88b5be
Libeedo <chadirvine@chadirvine.com>
2015-09-28 14:57:49 -0400

    little more

---
+++ Assets/Scripts/CageCount.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CageCount : MonoBehaviour {
+
+	private bool added = false;
+	
+	// Update is called once per frame
+	public bool AddToCage () {
+		if(!added){
+			added = true;
+			return added;
+			//Destroy(this);
+		}
+		return false;
+	}
+}
---
+++ Assets/Scripts/CageTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CageTrigger : MonoBehaviour {
+	private int bodyCount = 0;
+	public int maxCount = 5;
+	public GameObject targetObject;
+	void OnTriggerEnter2D (Collider2D col) 
+	{
+		if (col.tag == "Enemy") {
+			// ... find the Enemy script and call the Hurt function.
+			if(col.transform.root.GetComponent<CageCount> ().AddToCage()){
+				col.transform.root.GetComponent<Enemy>().Damaged(false,1000);
+				bodyCount++;
+				print ("BODY COUNT "+bodyCount);
+				if(bodyCount == maxCount){
+					targetObject.GetComponent<Animator>().SetTrigger("switchON");
+					Destroy (gameObject);
+				}
+			}
+
+		}
+	}
+}
